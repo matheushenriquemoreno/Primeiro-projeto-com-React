209079ebcf1c80886292ac7a006cefd811d9c680
Matheus Henrique <69221000+matheushenriquemoreno@users.noreply.github.com>
2022-08-03 21:43:51 -0300

    Adicionando componente footer

---
+++ organo/src/Componentes/Footer/index.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+
+const Footer = () => {
+
+    return (
+        <footer className='footer'>
+            <img src="/imagens/fundo.png" alt="aqui vai o fotter"></img>
+        </footer>
+    )
+}
+
+export default Footer
